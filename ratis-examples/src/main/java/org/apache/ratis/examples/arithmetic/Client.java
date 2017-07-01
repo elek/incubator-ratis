06263460af69f86a1fda4847678940f2e905ec6b
Elek, Márton <elek@users.noreply.github.com>
2017-07-01 19:27:58 +0200

    Client and Server class to execute a test cluster

---
+++ ratis-examples/src/main/java/org/apache/ratis/examples/arithmetic/Client.java
@@ -0,0 +1,44 @@
+package org.apache.ratis.examples.arithmetic;
+
+import org.apache.ratis.client.RaftClient;
+import org.apache.ratis.conf.Parameters;
+import org.apache.ratis.conf.RaftProperties;
+import org.apache.ratis.examples.arithmetic.expression.DoubleValue;
+import org.apache.ratis.examples.arithmetic.expression.Expression;
+import org.apache.ratis.examples.arithmetic.expression.Variable;
+import org.apache.ratis.grpc.GrpcFactory;
+import org.apache.ratis.protocol.*;
+
+/**
+ * Created by elek on 6/15/17.
+ */
+public class Client {
+  public static void main(String[] args) throws Exception {
+    new Client().run();
+  }
+
+  private void run() throws Exception {
+    RaftProperties raftProperties = new RaftProperties(true);
+
+    RaftClient.Builder builder =
+        RaftClient.newBuilder().setProperties(raftProperties);
+    RaftGroup raftGroup = new RaftGroup(RaftGroupId.createId(), new RaftPeer[] {
+        new RaftPeer(RaftPeerId.valueOf("node0"), "localhost:6000"),
+        new RaftPeer(RaftPeerId.valueOf("node1"), "localhost:6001")});
+
+    builder.setRaftGroup(raftGroup);
+    builder.setClientRpc(new GrpcFactory(new Parameters()).newRaftClientRpc());
+    RaftClient client = builder.build();
+    //    RaftClientReply asd = client
+    //        .send(new AssignmentMessage(new Variable("asd"), new
+    // DoubleValue(12d)));
+    RaftClientReply asd =
+        client.sendReadOnly(Expression.Utils.toMessage(new Variable("asd")));
+    System.out.println(asd.getMessage().getContent());
+    Expression response =
+        Expression.Utils.bytes2Expression(asd.getMessage().getContent().toByteArray(), 0);
+    System.out.println(((DoubleValue)response).toString());
+    System.exit(-1);
+
+  }
+}
---
+++ ratis-examples/src/main/java/org/apache/ratis/examples/arithmetic/Server.java
@@ -0,0 +1,65 @@
+package org.apache.ratis.examples.arithmetic;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.log4j.Level;
+import org.apache.ratis.client.RaftClient;
+import org.apache.ratis.conf.Parameters;
+import org.apache.ratis.conf.RaftProperties;
+import org.apache.ratis.grpc.GrpcConfigKeys;
+import org.apache.ratis.hadooprpc.HadoopConfigKeys;
+import org.apache.ratis.hadooprpc.server.HadoopRpcService;
+import org.apache.ratis.protocol.RaftGroup;
+import org.apache.ratis.protocol.RaftGroupId;
+import org.apache.ratis.protocol.RaftPeer;
+import org.apache.ratis.protocol.RaftPeerId;
+import org.apache.ratis.server.RaftServer;
+import org.apache.ratis.server.RaftServerConfigKeys;
+import org.apache.ratis.server.impl.RaftServerImpl;
+import org.apache.ratis.server.impl.ServerImplUtils;
+import org.apache.ratis.statemachine.StateMachine;
+import org.apache.ratis.util.LogUtils;
+
+import java.util.Collections;
+import java.util.stream.IntStream;
+
+/**
+ * Hello world!
+ */
+public class Server {
+  static {
+    LogUtils.setLogLevel(RaftServerImpl.LOG, Level.INFO);
+    LogUtils.setLogLevel(RaftClient.LOG, Level.INFO);
+  }
+
+  public static void main(String[] args) throws Exception {
+    new Server().run(args[0]);
+  }
+
+  private void run(String nodeIdx) throws Exception {
+    int idx = Integer.valueOf(nodeIdx);
+    RaftPeerId id = RaftPeerId.valueOf("node" + idx);
+    RaftProperties properties = new RaftProperties(true);
+    GrpcConfigKeys.Server.setPort(properties, 6000 + idx);
+    properties
+        .setInt(GrpcConfigKeys.OutputStream.RETRY_TIMES_KEY, Integer.MAX_VALUE);
+    //   Configuration conf = HadoopConfigKeys.getConf(new Parameters());
+    //   HadoopConfigKeys.setConf();
+    RaftServerConfigKeys.setStorageDir(properties, "/tmp/ratis" + nodeIdx);
+    //            properties.set(RaftServerConfigKeys.Rpc.TIMEOUT_MIN_KEY,
+    // "5000");
+    //            properties.set(RaftServerConfigKeys.Rpc.TIMEOUT_MAX_KEY,
+    // "6000");
+    StateMachine stateMachine = new ArithmeticStateMachine();
+    RaftGroupId raftGroupId = new RaftGroupId("test123412341234".getBytes());
+    RaftPeer[] raftPeers = IntStream.range(0, 3).mapToObj(
+        i -> new RaftPeer(RaftPeerId.valueOf("node" + i),
+            "localhost:" + (6000 + i))).toArray(RaftPeer[]::new);
+    RaftGroup raftGroup = new RaftGroup(raftGroupId, raftPeers);
+    RaftServer raftServer = RaftServer.newBuilder()
+        .setServerId(RaftPeerId.valueOf("node" + nodeIdx))
+        .setStateMachine(stateMachine).setProperties(properties)
+        .setPeers(raftGroup).build();
+    raftServer.start();
+
+  }
+}
